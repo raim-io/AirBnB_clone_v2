96f2c6385182407b8c5398ddf51d5d52156faaa5
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-07-31 02:12:56 +0100

    fix: pycodestyle errors

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,6 +11,7 @@ def hello():
     """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Display 'HBNB!'"""
@@ -18,4 +19,4 @@ def hbnb():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
